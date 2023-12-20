0828a1d715a9b8f2dd1c62a3d0fa8601825a6b7c
min950103 <min950103@kakao.com>
2023-12-20 10:15:17 +0900

    중복제거

--- Python/lotto.py
+++ Python/lotto.py
@@ -1,11 +1,18 @@
 # 로또복권
 import random
 
-lotto_number = []
-number = list(range(1,46))
+for j in range(5):
+    lotto_number = []
+    
+    for i in range(1,7):
+        number = list(range(1,46))
+        my_choice = random.choice(number)
+        
+        while my_choice in lotto_number:
+            my_choice =random.choice(number)
+            
+        lotto_number.append(my_choice)
+    
+    print(lotto_number)
 
-for i in range(1,7):
-    my_choice = random.choice(number)
-    lotto_number.append(my_choice)
 
-print(lotto_number)
